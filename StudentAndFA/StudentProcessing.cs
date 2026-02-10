3370849171e7a41281eb4ee7b97d1bf3e656bed4
Yaroslav Zobnin <zobnin114@gmail.com>
2026-02-10 21:16:42 +0300

    Extra lines of code have been removed

--- StudentAndFA/StudentProcessing.cs
+++ StudentAndFA/StudentProcessing.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StudentAndFA
+﻿namespace StudentAndFA
 {
     internal static class StudentProcessing
     {
